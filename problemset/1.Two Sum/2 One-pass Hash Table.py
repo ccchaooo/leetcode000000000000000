840ece2326d439279e16a0631a4a9b864fb8145c
chaodeng <645111208@qq.com>
2018-09-28 10:52:28 +0800

    update

--- problemset/1.Two Sum/2 One-pass Hash Table.py
+++ problemset/1.Two Sum/2 One-pass Hash Table.py
@@ -12,27 +12,16 @@ class Solution:
     def twoSum(self, nums, target):
         self.nums = nums
         self.target = target
-        items = [item(k, v) for k, v in enumerate(self.nums)]
-        items.sort(key=lambda x: x.v, reverse=False)
-        for i, x in enumerate(items):
-            j = i + 1
-            while j < len(items):
-                if x.v + items[j].v == target:
-                    ret = [x.k, items[j].k]
-                    return ret
-                elif x.v + items[j].v > target:
-                    break
-                j = j + 1
+        self.dc ={}
+        for i, x in enumerate(self.nums):
+            if self.target - x in self.dc:
+                ret = [self.dc.get(self.target - x), i]
+                return ret
+            self.dc[x] = i
+
         return 0
 
 
-class item(object):
-
-    def __init__(self, k, v):
-        self.k = k
-        self.v = v
-
-
-li = [3,2,4]
+li = [3, 2, 4]
 So = Solution()
-print(So.twoSum(li,5))
+print(So.twoSum(li, 5))
